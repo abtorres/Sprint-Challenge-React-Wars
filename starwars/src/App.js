3f82528170d4e4a72e1048edaa4158017e6cbd2a
abtorres <abtorres079@gmail.com>
2019-08-03 12:34:35 -0600

    fetch api/having issue no-unused-expressions

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,17 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
+import CharCard from './components/CharCard/CharCard';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-
+  const [chars, setChars] = useState()
+  // console.log(chars)
   // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  useEffect(() => {
+    axios.get('https://swapi.co/api/people')
+      .then(res => {
+        // console.log(res)
+        setChars(res.data.results)
+      })
+      .catch();
+  }, [])
 
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
+      {chars.map(char => {
+        <CharCard char={char} />
+      })}
     </div>
   );
 }
---
+++ starwars/src/components/CharCard/CharCard.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Card = styled.div`
+    color: green;
+`;
+const CharCard = (props) => {
+    console.log(props)
+    return (
+        <Card>
+
+        </Card>
+    )
+}
+
+export default CharCard;
